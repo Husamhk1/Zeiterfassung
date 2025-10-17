6771497d59e2ed32b9eec758e97202c330224700
TRADUIDE\husam.qasem <Husam.Qasem@tradui.de>
2025-10-18 00:08:56 +0200

    new

---
+++ Zeiterfassung/src/main/java/zeiterfasser/zeiterfassung/Zeiterfassung.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package zeiterfasser.zeiterfassung;
+
+/**
+ *
+ * @author husam.qasem
+ */
+public class Zeiterfassung {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Zeiterfassung!");
+    }
+}
