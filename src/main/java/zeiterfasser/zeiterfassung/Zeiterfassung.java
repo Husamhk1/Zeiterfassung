0b6d939f5701ed75af80a7a5eb8903bf5fce3411
TRADUIDE\husam.qasem <Husam.Qasem@tradui.de>
2025-11-03 14:20:58 +0100

    Test

--- src/main/java/zeiterfasser/zeiterfassung/Zeiterfassung.java
+++ src/main/java/zeiterfasser/zeiterfassung/Zeiterfassung.java
@@ -12,6 +12,7 @@ public class Zeiterfassung {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("zeiterfasser.zeiterfassung.Zeiterfassung.main()");
         
     }
 }
